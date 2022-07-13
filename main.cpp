fc19e3b13bdbb4af557a28146066d0dae7373585
abhishek1217 <abhishek95381@gmail.com>
2022-07-13 15:54:53 +0530

    Updation Done(Only City).

--- main.cpp
+++ main.cpp
@@ -9,30 +9,63 @@ class Cases {
         char state[20],city[20],total_cases[20],active_cases[20],recovered[20],deaths[20];
 };
 
-// void insert_cases(){
-//     Cases insert;
-//     fstream cases;
-//     int n;
-//     cases.open("cases.txt",ios::app);
-//     cout << "\nEnter the no of Records:  ";
-//     cin >> n;
-//     for(int i = 0;i < n; i++){
-//         cout << "\nEnter State Name: ";
-//         cin >> insert.state;
-//         cout << "\nEnter City: ";
-//         cin >> insert.city;
-//         cout << "\nEnter Total Cases: ";
-//         cin >> insert.total_cases;
-//         cout << "\nEnter Active Cases: ";
-//         cin >> insert.active_cases;
-//         cout << "\nEnter Recovered: ";
-//         cin >> insert.recovered;
-//         cout << "\nEnter Deaths: ";
-//         cin >> insert.deaths;
-//         cases << insert.state << "|" << insert.city << "|"<< insert.total_cases << "|"<< insert.active_cases << "|"<< insert.recovered << "|"<< insert.deaths << "\n";
-//     }
-// }
-void update(char *){
+int no_of_records(char s[100]){
+    fstream file;
+    file.open(s,ios::in);
+    int counter=0;
+    char buffer[120];
+    while(!file.eof()){
+        file.getline(buffer,120,'\n');
+        counter ++;
+    }
+    return counter;
+}
+void update_city(){
+    Cases update[2];
+    char s[100] = "city_data.txt";
+    int count = no_of_records(s);
+    cout << "\nEnter State: ";
+    cin >> update[0].state;
+    cout << "\nEnter City: ";
+    cin >> update[0].city;
+    cout << "\nEnter details for today\n";
+    cout << "\nEnter New Cases: ";
+    cin >> update[0].active_cases;
+    cout << "\nEnter Recovered: ";
+    cin >> update[0].recovered;
+    cout << "\nEnter Deaths: ";
+    cin >> update[0].deaths;
+
+    fstream city_update,temp;
+
+    city_update.open("city_data.txt",ios::in);
+    temp.open("cityout.txt",ios::app);
+    int total,active,recover,death;
+    while((count--)!=1){
+        city_update.getline(update[1].state,20,'|');
+        city_update.getline(update[1].city,20,'|');
+        city_update.getline(update[1].total_cases,20,'|');
+        city_update.getline(update[1].active_cases,20,'|');
+        city_update.getline(update[1].recovered,20,'|');
+        city_update.getline(update[1].deaths,20,'\n');
+        if(!strcmp(update[0].city,update[1].city)){
+            total = atoi(update[1].total_cases) + atoi(update[0].active_cases) ;
+            active = atoi(update[1].active_cases) - (atoi(update[0].recovered) + atoi(update[0].deaths));
+            recover = atoi(update[1].recovered) + atoi(update[0].recovered);
+            death = atoi(update[1].deaths) + atoi(update[0].deaths);
+            temp << update[1].state << "|" << update[0].city << "|" << total << "|" << active << "|" << recover << "|" << death << "\n";
+        }
+        else{
+        temp << update[1].state << "|" << update[1].city << "|" << update[1].total_cases << "|" << update[1].active_cases << "|" << update[1].recovered << "|" << update[1].deaths << "\n";
+        }
+    }
+    city_update.close();
+    temp.close();
+    remove("city_data.txt");
+    rename("cityout.txt","city_data.txt");
+}
+
+void update_state(char *state_name){
     cout << "Updated";
 }
 
@@ -41,7 +74,7 @@ void update(char *){
 void insert_cases(){
     Cases insert;
     fstream city,state,buff;
-    char s_name[20];
+    char s_name[20],buffer[80];
     int n;
     cout << "Enter number of records";
     city.open("city_data.txt",ios::app);
@@ -63,8 +96,11 @@ void insert_cases(){
         while (!state.eof())
         {
             state.getline(s_name,20,'|');
+            state.getline(buffer,80,'\n');
+            cout << s_name << " " << insert.state << endl;
             if(!(strcmp(s_name,insert.state))){
-                update(s_name);
+                update_state(s_name);
+                break;
             }
             else{
                 state.close();
@@ -74,23 +110,13 @@ void insert_cases(){
                 break;
             }
         }
-        city << s_name << "|" << insert.city << "|" << insert.total_cases << "|" << insert.active_cases << "|"<< insert.recovered << "|"<< insert.deaths << "\n";
+        city << insert.state << "|" << insert.city << "|" << insert.total_cases << "|" << insert.active_cases << "|"<< insert.recovered << "|"<< insert.deaths << "\n";
     }
     state.close();
     city.close();
 }
 
-int no_of_records(){
-    fstream file;
-    file.open("cases.txt",ios::in);
-    int counter=0;
-    char buffer[120];
-    while(!file.eof()){
-        file.getline(buffer,120,'\n');
-        counter ++;
-    }
-    return counter;
-}
+
 
 void display(){
     fstream filebuf;
@@ -120,11 +146,13 @@ int main(){
         }
     }
     while(1){
-    cout << "\n1. Insert Record\n4. Exit\n";
+    cout << "\n1. Insert Record\n2.Update City Details\n4. Exit\n";
     cout << "\nEnter your choice: ";
     cin >> choice;
     switch(choice){
         case 1: insert_cases();
+        break;
+        case 2: update_city();
         break;
         case 4: exit(1);
         default: cout << "\nInvalid Choice";
--- sample.cpp
+++ sample.cpp
@@ -5,15 +5,24 @@
 using namespace std;
 
 int main(){
-    fstream file;
-    fstream file2;
-    file.open("cases.txt",ios::in);
-    file2.open("cases.txt",ios::out);
-    char some[20];
-    char buffer[100];
-    while(!file.eof()){
-        file.getline(some,20,'|');
-        cout << some << " ";
-    }
-    file2 << "something";
+//     fstream file;
+//     fstream file2;
+//     file.open("cases.txt",ios::in);
+//     file2.open("cases.txt",ios::out);
+//     char some[20];
+//     char buffer[100];
+//     while(!file.eof()){
+//         file.getline(some,20,'|');
+//         cout << some << " ";
+//     }
+//     file2 << "something";
+    // char *s = "sampl1.txt";
+    // char *s1 = "sampl2.txt";
+    // /*	Deletes the file if exists */
+	// if (rename(s, s1) != 0)
+	// 	perror("Error renaming file");
+	// else
+	// 	cout << "File renamed successfully";
+    int a = 10;
+    cout << (char *)a;
 }
